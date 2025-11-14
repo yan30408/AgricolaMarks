aa9f7679eb7b6c19523bd787c4a0f3349d330fcf
yan3 <yan3.0408@gmail.com>
2025-11-14 17:58:59 +0900

    chore: 環境・共通設定の整備 改行コード統一 エミュレーター設定 Firestore ルール・インデックスの調整 npm スクリプト追加

--- src/initializer.js
+++ src/initializer.js
@@ -12,14 +12,23 @@ if (!firebase.apps.length) {
 }
 
 export const db = firebase.firestore();
-// export const rdb = firebase.database();
+if (process.env.REACT_APP_USE_FIRESTORE_EMULATOR === "true") {
+  db.useEmulator("localhost", 8080);
+}
 export const auth = firebase.auth();
+if (process.env.REACT_APP_USE_AUTH_EMULATOR === "true") {
+  auth.useEmulator("http://localhost:9099", { disableWarnings: true });
+}
 export const providers = {
   twitter: new firebase.auth.TwitterAuthProvider()
 };
 export const storage = firebase.storage();
-export const functions = firebase.functions();
+export const functions = firebase.app().functions("asia-northeast1");
+if (process.env.REACT_APP_USE_FUNCTIONS_EMULATOR === "true") {
+  functions.useEmulator("localhost", 5001);
+}
 export const FieldValue = firebase.firestore.FieldValue;
 export const Timestamp = firebase.firestore.Timestamp;
+export const FieldPath = firebase.firestore.FieldPath;
 
 auth.useDeviceLanguage();
